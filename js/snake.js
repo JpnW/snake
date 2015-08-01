abb1a77ab16d466f985243bd31430e25d5897818
JpnW <jenncoding@gmail.com>
2015-08-01 13:33:40 -0400

    finish snake.js

--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,13 @@
+//API
+// var b = new Board(11,11);
+//var matrix = b.matrix;
+// b.changeSnakeDir('up'); this can be called several times
+// var matrix = b.step();
+// b.changeSnakeDir('down'); this can be called several times
+// var matrix = b.step();
+// b.changeSnakeDir('left'); this can be called several times
+// var matrix = b.step();
+
 (function() {
   if (typeof SG === "undefined") {
     // window.SG = {};
@@ -11,15 +21,10 @@
    |
    y
 */
-
-  var Apple = SG.Apple = function() {
-
-  };
-
-
-
-  var Snake = SG.Snake = function(){
-    this.body = [];
+  var Snake = SG.Snake = function(x, y){
+    if(typeof x !== "undefined" && typeof y !== "undefined"){
+      this.body = [[x, y]];
+    }
     this.head_dir = 'up';
   };
 
@@ -38,7 +43,6 @@
     if(this.body.length > 0) {
       //drop last element
       //unshift first element with new
-      console.log(this.body);
       var newHead = this.addPoints(this.body[0], Snake.DIRECTIONS[this.head_dir]);
       this.body.unshift(newHead);
       this.body.pop();
@@ -53,19 +57,104 @@
     }
   };
 
+  Snake.prototype.nextMove = function() {
+    var nextMove = this.addPoints(this.body[0], Snake.DIRECTIONS[this.head_dir]);
+    return nextMove;
+  };
+
+
   var Board = SG.Board = function(width, height){
-    this.apple = new Apple();
-    this.snake = new Snake();
+    var x = Math.floor(width / 2);
+    var y = Math.floor(height / 2);
+    this.snake = new Snake(x, y);
     this.width = width;
     this.height = height;
+    this.drawMatrix();
+    this.generateApple();
+    this.gameOver = false;
+  };
+
+  Board.prototype.drawMatrix = function() {
+    //draw empty space with 0
+    //draw snake with 1
+    //if apple exist draw apple with 2
+    var matrix = [];
+    for (var i=0; i<this.height; i++) {
+      matrix.push([]);
+      for (var j=0; j<this.width; j++) {
+        matrix[i].push(0);
+      }
+    }
+    var snake_body = this.snake.body;
+    for (var i=0; i< snake_body.length; i++) {
+      var x = snake_body[i][0];
+      var y = snake_body[i][1];
+      matrix[y][x] = 1;
+    };
+    if(this.apple){
+      var x = this.apple[0];
+      var y = this.apple[1];
+      matrix[y][x] = 2;
+    }
+    this.matrix = matrix;
   };
 
   Board.prototype.generateApple = function() {
-    
+    //draw apple with 2
+    //assign this.apple to generated location
+    var randomY =  Math.floor(Math.random() * this.height);
+    var randomX = Math.floor(Math.random() * this.width);
+    if (this.matrix[randomY][randomX] === 0) {
+      this.matrix[randomY][randomX] = 2;
+      this.apple = [randomX, randomY];
+    } else {
+      this.generateApple();
+    }
   };
 
+  Board.prototype.changeSnakeDir = function(dir) {
+    this.snake.head_dir = dir;
+  };
 
-  Board.prototype.step = function() {};
+  Board.prototype.died = function() {
+    var head = this.snake.body[0];
+    this.snake.body.forEach(function(position, index) {
+      if(index !== 0 ) {
+        var x = position[0];
+        var y = position[1];
+        if(head[0] === x && head[1] === y) {
+          console.log('here')
+          return true;
+        }
+      }
+    });
 
+    return (head[0] < 0 || (head[0] > this.width-1) || head[1] < 0 || (head[1] > this.height-1));
+  };
 
+  Board.prototype.step = function() {
+    //check next move is apple
+    //yes: snake eat, generateApple
+    //no: snake move
+    //check if dead? return "dead"
+    //if not dead, return generated matrix
+    if(this.gameOver) return "dead";
+    var nextMove = this.snake.nextMove();
+    var x = nextMove[0];
+    var y = nextMove[1];
+    if (this.matrix[y] && this.matrix[y][x] && this.matrix[y][x] === 2) {
+      this.snake.eat();
+      this.generateApple();
+    } else {
+      this.snake.move();
+    }
+
+    if (!this.died()) {
+      this.drawMatrix();
+      return this.matrix;
+    } else{
+      this.gameOver = true;
+      return "dead"
+    }
+  };
 })();
--- test/snake.spec.js
+++ test/snake.spec.js
@@ -2,6 +2,7 @@ require('../js/snake.js');
 var expect = require('chai').expect;
 
 var Snake = SG.Snake;
+var Board = SG.Board;
 
 describe('snake', function(){
   beforeEach(function(){
@@ -15,10 +16,25 @@ describe('snake', function(){
     expect(output).to.eql([3,3]);
   });
 
+  it('can show next move', function(){
+    var s = new Snake();
+    s.body = [[1,1]];
+    var nm = s.nextMove();
+    expect(nm).to.eql([1,0]);
+  });
+
   it('can move', function(){
     var s = new Snake();
     s.body = [[1,1]];
     s.move();
     expect(s.body).to.eql([[1,0]]);
+  });
+});
+
+describe('broad', function(){
+  it('can draw matrix', function(){
+    var b = new Board(3,3);
+    var matrix = b.drawMatrix();
+    expect(matrix).to.eql([[0,0,0],[0,1,0],[0,0,0]])
   });
 });
